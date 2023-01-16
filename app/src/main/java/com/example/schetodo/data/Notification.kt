e0dd91e4b3e80be1274b0aab297ff45943b2e179
Mike <menzelmike123@gmail.com>
2023-01-16 21:44:21 +0100

    add Notification entity and foreign key to TodoBlock

---
+++ app/src/main/java/com/example/schetodo/data/Notification.kt
@@ -0,0 +1,22 @@
+package com.example.schetodo.data
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import java.time.LocalTime
+
+@Entity(
+    primaryKeys = ["time", "todoBlockId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = TodoBlock::class,
+            parentColumns = ["todoBlockId"],
+            childColumns = ["todoBlockId"],
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE
+        )
+    ]
+)
+data class Notification(
+    val time: LocalTime,
+    val todoBlockId: Int
+)
