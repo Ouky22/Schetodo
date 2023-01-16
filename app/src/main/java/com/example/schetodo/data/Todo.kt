338e7764596be6a200ce9d46dd46479c464df649
Mike <menzelmike123@gmail.com>
2023-01-16 20:27:07 +0100

    add Todo entity

---
+++ app/src/main/java/com/example/schetodo/data/Todo.kt
@@ -0,0 +1,22 @@
+package com.example.schetodo.data
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo(
+    @PrimaryKey(autoGenerate = true) val todoId: Int,
+    val description: String?,
+    val priority: TodoPriority,
+    val flag: TodoFlag,
+    val categoryId: Int // TODO create foreign key constraint
+)
+
+enum class TodoFlag {
+    UNDONE, IN_PROGRESS, DONE, RECURRING
+}
+
+enum class TodoPriority {
+    LOW, MEDIUM, HIGH, VERY_HIGH
+}
