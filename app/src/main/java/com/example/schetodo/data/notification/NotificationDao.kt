a3db2bd1d4c8f0472e7febb9099917dbfa6feae0
Mike <menzelmike123@gmail.com>
2023-02-26 22:54:45 +0100

    add basic NotificationDao

---
+++ app/src/main/java/com/example/schetodo/data/notification/NotificationDao.kt
@@ -0,0 +1,22 @@
+package com.example.schetodo.data.notification
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NotificationDao {
+    @Query("SELECT * FROM Notification")
+    fun getAllNotifications(): Flow<List<Notification>>
+
+    @Query("SELECT * FROM Notification WHERE todoBlockId = :todoBlockId")
+    fun getNotificationsOfTodoBlock(todoBlockId: Int): Flow<List<Notification>>
+
+    @Insert
+    suspend fun insertNotification(notification: Notification): Long
+
+    @Delete
+    suspend fun deleteNotification(notification: Notification)
+}
