be211a7565ce33d44daf4eb221d18fa6b6af693b
Mike <menzelmike123@gmail.com>
2023-02-26 21:50:45 +0100

    add auto generated id to notification entity
    so that it is easier to edit a notification

--- app/src/main/java/com/example/schetodo/data/notification/Notification.kt
+++ app/src/main/java/com/example/schetodo/data/notification/Notification.kt
@@ -3,11 +3,11 @@ package com.example.schetodo.data.notification
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
+import androidx.room.PrimaryKey
 import com.example.schetodo.data.todo_block.TodoBlock
 import java.time.LocalTime
 
 @Entity(
-    primaryKeys = ["time", "todoBlockId"],
     foreignKeys = [
         ForeignKey(
             entity = TodoBlock::class,
@@ -20,6 +20,7 @@ import java.time.LocalTime
     indices = [Index("todoBlockId")]
 )
 data class Notification(
+    @PrimaryKey(autoGenerate = true) val notificationId: Int,
     val time: LocalTime,
     val todoBlockId: Int
 )
