9644b82f0293eca9cab9c9ed7a98d5a17b59a5f8
Mike <menzelmike123@gmail.com>
2023-03-02 20:04:50 +0100

    schedule next notification in NotificationBroadcastReceiver

--- app/src/main/java/com/example/schetodo/ui/feature/schedule/notification/NotificationBroadcastReceiver.kt
+++ app/src/main/java/com/example/schetodo/ui/feature/schedule/notification/NotificationBroadcastReceiver.kt
@@ -4,9 +4,23 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class NotificationBroadcastReceiver : BroadcastReceiver() {
+    
+    @Inject
+    lateinit var todoBlockNotificationScheduler: TodoBlockNotificationScheduler
+    
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "notification", Toast.LENGTH_LONG).show()
+
+        CoroutineScope(IO).launch {
+            todoBlockNotificationScheduler.scheduleNextNotificationIfExists()
+        }
     }
 }
