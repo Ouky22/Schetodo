662b3e8cb97710b48b16ab93908d347e67df16d4
Mike <menzelmike123@gmail.com>
2023-03-03 11:03:05 +0100

    add notification channel for schedule notifications

--- app/src/main/java/com/example/schetodo/SchetodoApp.kt
+++ app/src/main/java/com/example/schetodo/SchetodoApp.kt
@@ -1,7 +1,34 @@
 package com.example.schetodo
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.compose.ui.graphics.toArgb
+import com.example.schetodo.ui.theme.md_theme_dark_primary
+import com.example.schetodo.ui.util.NotificationService
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SchetodoApp : Application()
+class SchetodoApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            NotificationService.SCHEDULE_NOTIFICATION_CHANNEL_ID,
+            getString(R.string.schedule_block_notification),
+            NotificationManager.IMPORTANCE_DEFAULT,
+        )
+        channel.description = getString(R.string.schedule_notification_description)
+        channel.enableLights(true)
+        channel.lightColor = md_theme_dark_primary.toArgb()
+
+        with(getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager) {
+            createNotificationChannel(channel)
+        }
+    }
+}
