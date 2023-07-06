bd165ac886766f7f885ef82aa18fa12842bdfee1
Ouky22 <Menzelmike123@gmail.com>
2023-07-06 22:37:42 +0200

    add basic ScheduleTemplateviewModel

---
+++ app/src/main/java/com/example/schetodo/ui/feature/schedule/schedule_template/ScheduleTemplateViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.schetodo.ui.feature.schedule.schedule_template
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.schetodo.data.schedule_template.ScheduleTemplate
+import com.example.schetodo.data.schedule_template.ScheduleTemplateRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ScheduleTemplateViewModel @Inject constructor(
+    scheduleTemplateRepository: ScheduleTemplateRepository
+) : ViewModel() {
+
+    private val _scheduleTemplates = MutableStateFlow<List<ScheduleTemplate>>(emptyList())
+    val scheduleTemplates: StateFlow<List<ScheduleTemplate>>
+        get() = _scheduleTemplates.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            scheduleTemplateRepository.getAll().collect { scheduleTemplates ->
+                _scheduleTemplates.value = scheduleTemplates
+            }
+        }
+    }
+}
