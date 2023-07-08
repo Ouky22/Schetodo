1f34e39e469e696ddce87565b9cdfea088b99593
Ouky22 <Menzelmike123@gmail.com>
2023-07-08 16:47:13 +0200

    change look of ElementPickerDialog

--- app/src/main/java/com/example/schetodo/ui/components/ElementPickerDialog.kt
+++ app/src/main/java/com/example/schetodo/ui/components/ElementPickerDialog.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,9 +29,9 @@ fun <T> ElementPickerDialog(
     Dialog(
         onDismissRequest = { onDismiss() }
     ) {
-        Surface {
+        Card(modifier = modifier) {
             Column(
-                modifier = modifier.padding(vertical = 16.dp),
+                modifier = Modifier.padding(vertical = 16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
