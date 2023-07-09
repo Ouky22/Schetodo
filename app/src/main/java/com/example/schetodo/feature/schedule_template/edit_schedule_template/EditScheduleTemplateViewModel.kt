6358854f32a113a0bbe68c8d567a3f01188a7711
Ouky22 <Menzelmike123@gmail.com>
2023-07-09 19:21:51 +0200

    add logic to load schedule blocks for schedule template

--- app/src/main/java/com/example/schetodo/feature/schedule_template/edit_schedule_template/EditScheduleTemplateViewModel.kt
+++ app/src/main/java/com/example/schetodo/feature/schedule_template/edit_schedule_template/EditScheduleTemplateViewModel.kt
@@ -1,16 +1,39 @@
 package com.example.schetodo.feature.schedule_template.edit_schedule_template
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.schetodo.data.schedule_block.ScheduleBlockRepository
+import com.example.schetodo.feature.use_case.ConvertScheduleBlocksToScheduleListItemsUseCase
+import com.example.schetodo.feature.use_case.GeneralUseCases
+import com.example.schetodo.ui.navigation.schedule.EditScheduleTemplate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EditScheduleTemplateViewModel @Inject constructor() : ViewModel() {
+class EditScheduleTemplateViewModel @Inject constructor(
+    private val scheduleBlockRepository: ScheduleBlockRepository,
+    private val generalUseCases: GeneralUseCases,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
     private val _state = MutableStateFlow(EditScheduleTemplateState())
     val state: StateFlow<EditScheduleTemplateState>
         get() = _state.asStateFlow()
+
+    init {
+        val templateId: Int = savedStateHandle[EditScheduleTemplate.scheduleTemplateIdArg]
+            ?: throw Exception("No schedule template id provided")
+
+        viewModelScope.launch {
+            scheduleBlockRepository.getScheduleBlocksOfScheduleTemplate(templateId).collect {
+                val scheduleListItems = generalUseCases.convertScheduleBlocksToScheduleListItems(it)
+                _state.value = _state.value.copy(scheduleItems = scheduleListItems)
+            }
+        }
+    }
 }
