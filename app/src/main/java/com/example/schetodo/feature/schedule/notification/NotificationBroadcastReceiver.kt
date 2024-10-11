1ed88b35610a533f10f0b4d404246c1c9cc7e211
Ouky22 <72163389+Ouky22@users.noreply.github.com>
2024-10-11 21:18:58 +0200

    fix: execute notification task in BroadcastReceiver with goAsync

--- app/src/main/java/com/example/schetodo/feature/schedule/notification/NotificationBroadcastReceiver.kt
+++ app/src/main/java/com/example/schetodo/feature/schedule/notification/NotificationBroadcastReceiver.kt
@@ -3,12 +3,12 @@ package com.example.schetodo.feature.schedule.notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.example.schetodo.data.notification.NotificationRepository
 import com.example.schetodo.data.schedule_block.ScheduleBlockRepository
 import com.example.schetodo.ui.util.NotificationService
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,30 +22,36 @@ class NotificationBroadcastReceiver : BroadcastReceiver() {
     @Inject
     lateinit var notificationRepository: NotificationRepository
 
+    @Inject
+    lateinit var notificationScheduler: TodoBlockNotificationScheduler
+
 
     override fun onReceive(context: Context, intent: Intent) {
         val notificationId = intent.getIntExtra(NOTIFICATION_ID_EXTRA_KEY, -1)
         if (notificationId <= 0)
             return
 
-        MainScope().launch {
-            val notification =
-                notificationRepository.getNotificationById(notificationId).first() ?: return@launch
+        val pendingResult = goAsync()
+        CoroutineScope(SupervisorJob()).launch {
+            try {
+                val notification = notificationRepository
+                    .getNotificationById(notificationId).first() ?: return@launch
 
-            val scheduleBlock = scheduleBlockRepository.getScheduleBlockByTodoBlockId(
-                notification.todoBlockId
-            ).first() ?: return@launch
+                val scheduleBlock = scheduleBlockRepository.getScheduleBlockByTodoBlockId(
+                    notification.todoBlockId
+                ).first() ?: return@launch
 
-            val notificationService = NotificationService(context)
-            notificationService.showScheduleNotification(
-                scheduleBlock,
-                scheduleBlock.todoBlock.todoBlockId
-            )
+                val notificationService = NotificationService(context)
+                notificationService.showScheduleNotification(
+                    scheduleBlock,
+                    scheduleBlock.todoBlock.todoBlockId
+                )
 
-            notificationRepository.deleteNotification(notification)
-
-            // when the app is started an instance of TodoBlockNotificationScheduler is created and
-            // the next notification (if exists) is scheduled
+                notificationRepository.deleteNotification(notification)
+                notificationScheduler.scheduleNextNotificationIfExists()
+            } finally {
+                pendingResult.finish()
+            }
         }
     }
 }
