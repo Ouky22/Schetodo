b9cc85c1a3cbbd2db6192ac815194140f357ff93
Ouky22 <72163389+Ouky22@users.noreply.github.com>
2025-06-17 20:03:56 +0200

    fix: disable system navigation bar contrast
    so that it is not white when in dark mode

--- app/src/main/java/com/example/schetodo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/schetodo/ui/theme/Theme.kt
@@ -1,4 +1,5 @@
 package com.example.schetodo.ui.theme
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,8 +9,10 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -266,6 +269,14 @@ fun SchetodoTheme(
 
         darkTheme -> darkScheme
         else -> lightScheme
+    }
+
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as Activity).window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setNavigationBarContrastEnforced(false)
+        }
     }
 
     MaterialTheme(
